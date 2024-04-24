8e6d2e6ee63301dbc4a389e43bfa45671e66a81a
MuhammadAhmed-Developer <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2024-04-24 12:51:31 +0500

    build fixing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Editor from "@/components/Editor/Editor";
 
 const Home = () => {
   return (
-    <div className=" my-[50px] w-full">
+    <div className="my-[50px] w-full">
       <h1 className="text-red-700 font-bold text-[30px] text-center">Unial Admin</h1>
       <div className="my-[50px]">
         <Editor/>
--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,37 +1,5 @@
 "use client"
 import React, { useRef, useEffect, useState } from "react";
-import EditorJS from "@editorjs/editorjs";
-
-// Import Editor.js tools dynamically
-const importTools = async () => {
-  const Header = (await import("@editorjs/header")).default;
-  const Table = (await import("@editorjs/table")).default;
-  const List = (await import("@editorjs/list")).default;
-  const LinkTool = (await import("@editorjs/link")).default;
-  const RawTool = (await import("@editorjs/raw")).default;
-  const SimpleImage = (await import("@editorjs/simple-image")).default;
-  const Checklist = (await import("@editorjs/checklist")).default;
-  const Embed = (await import("@editorjs/embed")).default;
-  const Quote = (await import("@editorjs/quote")).default;
-  const Paragraph = (await import("@editorjs/paragraph")).default;
-  const ImageTool = (await import("@editorjs/image")).default;
-
-  return {
-    header: {
-      class: Header,
-      inlineToolbar: ["marker", "link", "bold", "italic"],
-    },
-    table: Table,
-    list: List,
-    linkTool: LinkTool,
-    raw: RawTool,
-    image: SimpleImage,
-    checklist: { class: Checklist, inlineToolbar: true },
-    embed: Embed,
-    quote: Quote,
-    paragraph: { class: Paragraph, inlineToolbar: true },
-  };
-};
 
 export default function Editor() {
   const [editorLoaded, setEditorLoaded] = useState(false);
@@ -39,13 +7,40 @@ export default function Editor() {
 
   useEffect(() => {
     const initializeEditor = async () => {
-      const tools = await importTools();
+      const EditorJS = (await import("@editorjs/editorjs")).default;
+      const Header = (await import("@editorjs/header")).default;
+      const Table = (await import("@editorjs/table")).default;
+      const List = (await import("@editorjs/list")).default;
+      const LinkTool = (await import("@editorjs/link")).default;
+      const RawTool = (await import("@editorjs/raw")).default;
+      const SimpleImage = (await import("@editorjs/simple-image")).default;
+      const Checklist = (await import("@editorjs/checklist")).default;
+      const Embed = (await import("@editorjs/embed")).default;
+      const Quote = (await import("@editorjs/quote")).default;
+      const Paragraph = (await import("@editorjs/paragraph")).default;
+      const ImageTool = (await import("@editorjs/image")).default;
+
+      const tools = {
+        header: {
+          class: Header,
+          inlineToolbar: ["marker", "link", "bold", "italic"],
+        },
+        table: Table,
+        list: List,
+        linkTool: LinkTool,
+        raw: RawTool,
+        image: SimpleImage,
+        checklist: { class: Checklist, inlineToolbar: true },
+        embed: Embed,
+        quote: Quote,
+        paragraph: { class: Paragraph, inlineToolbar: true },
+      };
 
       const editorInstance = new EditorJS({
-        holder: "editorjs-container", // Unique ID for the container element
+        holder: "editorjs-container",
         autofocus: true,
         placeholder: "Let`s write an awesome story!",
-        inlineToolbar: true, // Enable inline toolbar
+        inlineToolbar: true,
         tools,
       });
 
@@ -79,8 +74,8 @@ export default function Editor() {
     <div className="flex justify-center flex-col mx-auto w-[70%]">
       <div
         id="editorjs-container"
-        className=" min-h-[50vh] min-w-[80%] border-[1px] rounded-md"
-        style={{border:"1px solid lightgray", borderRadius:"10px", padding:"20px"}}
+        className="min-h-[50vh] min-w-[80%] border-[1px] rounded-md"
+        style={{border:"1px solid lightgray", padding:"20px"}}
       />
       <div className="flex justify-center my-[20px]">
         <button
