0f12f0694aa1bb8da4cb6803d15f9c84d8e59805
MuhammadAhmed-Developer <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2024-04-27 11:48:39 +0500

    Spacific component Api

---
+++ app/api/getComp/route.tsx
@@ -0,0 +1,19 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+export async function GET(req: any) {
+  try {
+    const componentName = req.nextUrl.searchParams.get("componentName");
+   
+    console.log("componentName", componentName);
+
+    const results = await sql`
+      SELECT * FROM Pages
+      WHERE block->> 'componentName' = ${componentName}
+      ORDER BY id ASC;
+    `;
+
+    return NextResponse.json({ results }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
